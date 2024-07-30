3ffc3d9a516902492f2195451c784157d7a9a9e0
ankthba <anikethdb1@gmail.com>
2024-07-30 19:21:28 -0400

    app is fully functional (YAYY :3)

---
+++ Config.swift
@@ -0,0 +1,15 @@
+//
+//  Config.swift
+//  OCR App iOS
+//
+//  Created by Aniketh Bandlamudi on 7/30/24.
+//
+
+import Foundation
+
+struct Config {
+    static let predictionKey = "e0862ead2b974151b32bb7e8fa4e4cfc"
+    static let endpoint = "https://visionmodelocr.cognitiveservices.azure.com/"
+    static let projectID = "657bdfbb-125e-4e72-9120-0067ed05ce8d"
+    static let publishedModelName = "Oral Cancer Radar (1)"
+}
