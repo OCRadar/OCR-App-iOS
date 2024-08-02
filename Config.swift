abc7b55d5f78a68dfa19a8f6c9c04d025a7ea942
ankthba <anikethdb1@gmail.com>
2024-08-02 13:22:47 -0400

    finalized for first testflight beta

--- Config.swift
+++ Config.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Config {
     static let predictionKey = "e0862ead2b974151b32bb7e8fa4e4cfc"
-    static let endpoint = "https://visionmodelocr.cognitiveservices.azure.com/"
+    static let endpoint = "https://visionmodelocr.cognitiveservices.azure.com/customvision/v3.0/Prediction/\\657bdfbb-125e-4e72-9120-0067ed05ce8d/classify/iterations/\\628e2a7c-241e-4158-acf8-470ee9e1f7fb/image"
     static let projectID = "657bdfbb-125e-4e72-9120-0067ed05ce8d"
     static let publishedModelName = "Oral Cancer Radar (1)"
 }
